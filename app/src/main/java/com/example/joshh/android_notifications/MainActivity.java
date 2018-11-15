e32d931168f0362c88f2378647bbe24859867d90
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-11-15 13:17:40 -0800

    Set the content intent to a new pending intent. Added a fullscreen activity.

--- app/src/main/java/com/example/joshh/android_notifications/MainActivity.java
+++ app/src/main/java/com/example/joshh/android_notifications/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.joshh.android_notifications;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     NotificationManager notificationManager;
 
     private Button getNotificationButton;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
         getNotificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(context, FullscreenActivity.class);
+                intent.putExtra("notification", "Notification Tapped");
+                pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
                 String channelId = getPackageName() + getString(R.string.channel_id_suffix);
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
                     CharSequence name = getString(R.string.notification_channel_name);
@@ -45,11 +52,12 @@ public class MainActivity extends AppCompatActivity {
                 }
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
                         .setPriority(NotificationManager.IMPORTANCE_HIGH)
-                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                         .setContentTitle("High importance")
                         .setContentText("This notification was created by a button")
-                        .setColor(getResources().getColor(R.color.colorPrimaryDark))
-                        .setDefaults(NotificationCompat.DEFAULT_VIBRATE);
+                        .setColor(getResources().getColor(R.color.colorAccent))
+                        .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
+                        .setContentIntent(pendingIntent);
                 notificationManager.notify(NOTIFICATION_ID_INSTANT, builder.build());
             }
         });
