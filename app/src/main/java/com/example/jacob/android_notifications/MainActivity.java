2363724fa9143c1f8e2b0d53277996377afd6ef1
Jacob Seppanen <jbseppanen@gmail.com>
2018-11-19 19:28:07 -0800

    Notifications working.

--- app/src/main/java/com/example/jacob/android_notifications/MainActivity.java
+++ app/src/main/java/com/example/jacob/android_notifications/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.jacob.android_notifications;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int BUTTON_NOTIFICATION_ID = 354;
+    NotificationManager notificationManager;
+    Context context;
+    int notificationImportance = NotificationManager.IMPORTANCE_DEFAULT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        findViewById(R.id.button_notify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String channelId = getPackageName() + ".button";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    CharSequence name = "Notify Button Channel";
+                    String description = "Notifications triggered by button";
+                    NotificationChannel channel = new NotificationChannel(channelId, name, notificationImportance);
+                    channel.setDescription(description);
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
+                        .setPriority(notificationImportance)
+                        .setContentTitle("Button")
+                        .setContentText("The button was pressed")
+                        .setColor(context.getColor(R.color.colorPrimary))
+                        .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                        .setDefaults(Notification.DEFAULT_ALL);
+                notificationManager.notify(BUTTON_NOTIFICATION_ID, builder.build());
+            }
+        });
+
     }
+
+
 }
