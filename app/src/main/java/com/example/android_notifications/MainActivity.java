b1a55976eab97a921032d3e67fb4831036a6ae10
Kyle Cole <kyle@misfitselectronics.com>
2019-03-28 16:45:36 -0400

    part 3 done, starting part 4

--- app/src/main/java/com/example/android_notifications/MainActivity.java
+++ app/src/main/java/com/example/android_notifications/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android_notifications;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,31 +23,35 @@ public class MainActivity extends AppCompatActivity {
         final Context context = this;
         Button notificationButton = findViewById(R.id.notification_button);
         final NotificationManager notificationManager =(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(
-                    CHANNEL_ID,
-                    Constants.notificationName,
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.setDescription(Constants.NOTIFICATION_DESCRIPTION);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
 
 
-
-                notificationButton.setOnClickListener(new View.OnClickListener() {
-
+        notificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent goToFullScreen = new Intent(context,FullscreenActivity.class);
+                goToFullScreen.putExtra(FullscreenActivity.FULL_SCREEN_STRING_1,"Notification Tapped");
+                PendingIntent pendingIntent = PendingIntent.getActivity(context,0,goToFullScreen,PendingIntent.FLAG_ONE_SHOT);
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel = new NotificationChannel(
+                            CHANNEL_ID,
+                            Constants.notificationName,
+                            NotificationManager.IMPORTANCE_DEFAULT);
+
+                    notificationChannel.setDescription(Constants.NOTIFICATION_DESCRIPTION);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    NotificationCompat.Builder builder =
+                            new NotificationCompat.Builder(context, CHANNEL_ID)
+                                    .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+                                    .setContentTitle(Constants.CONTENT_TITLE)
+                                    .setSmallIcon(Constants.CONTENT_ICON)
+                                    .setAutoCancel(true)
+                                    .setContentIntent(pendingIntent);
+
+                    notificationManager.notify(REMINDER_NOTIFICATION_ID, builder.build());
+                }
 
 
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                        .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
-                        .setContentTitle(Constants.CONTENT_TITLE)
-                        .setSmallIcon(Constants.CONTENT_ICON);
-
-
-                notificationManager.notify(REMINDER_NOTIFICATION_ID, builder.build());
 
             }
 
