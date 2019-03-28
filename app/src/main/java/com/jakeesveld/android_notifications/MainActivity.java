0df7a6e156d3804970fe5d8fdf7eb9f10370a15c
Jake Esveld <thejakeesveld@gmail.com>
2019-03-28 13:01:28 -0700

    Add functionality to second button

--- app/src/main/java/com/jakeesveld/android_notifications/MainActivity.java
+++ app/src/main/java/com/jakeesveld/android_notifications/MainActivity.java
@@ -3,7 +3,9 @@ package com.jakeesveld.android_notifications;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public static final int NOTIFICATION_ID = 526;
+    public static final int PENDING_INTENT_REQUEST_CODE = 786;
+    public static final int COOLER_INTENT_REQUEST_CODE = 5948;
     Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +50,54 @@ public class MainActivity extends AppCompatActivity {
                         .setDefaults(Notification.DEFAULT_ALL);
 
                 notificationManager.notify(NOTIFICATION_ID, builder.build());
+            }
+        });
+
+
+        findViewById(R.id.button_send_notification_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String channelID = getPackageName() + "button";
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    CharSequence name = "Notifications";
+                    String description = "These are the notifications we send you";
+                    int importance = NotificationManager.IMPORTANCE_HIGH;
+
+                    NotificationChannel channel = new NotificationChannel(channelID, name, importance);
+                    channel.setDescription(description);
+
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                Intent clickIntent = new Intent(context, FullscreenActivity.class);
+                clickIntent.putExtra("String", "This is a string");
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        context,
+                        PENDING_INTENT_REQUEST_CODE,
+                        clickIntent,
+                        PendingIntent.FLAG_ONE_SHOT
+                );
+
+                Intent actionIntent = new Intent(context, FullscreenActivity.class);
+                actionIntent.putExtra("String", "This is a cooler string.");
+                PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                        context,
+                        COOLER_INTENT_REQUEST_CODE,
+                        actionIntent,
+                        PendingIntent.FLAG_ONE_SHOT
+                );
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelID)
+                        .setContentTitle("Another Notification")
+                        .setContentText("This one is even cooler! Click here!")
+                        .setColor(getResources().getColor(R.color.colorAccent))
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setPriority(NotificationManager.IMPORTANCE_HIGH)
+                        .setContentIntent(pendingIntent)
+                        .addAction(R.drawable.ic_launcher_foreground, "No Click Me! I'm cooler", actionPendingIntent);
+
+                notificationManager.notify(NOTIFICATION_ID, builder.build());
+
             }
         });
     }
