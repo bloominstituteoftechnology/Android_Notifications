7cd7c2642c17de3bc23d4de8990051b62c29a58a
BL-CoderOvertime <brandonlively@live.com>
2019-03-28 14:26:13 -0400

    Linked Notification button to FullScreen Activity

--- app/src/main/java/com/example/myapplication/Constants.java
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
 public class Constants {
-    public static final int NOTIFY_ID = 1;
+    public static final int NOTIFY_ID = 18568;
+    public static final int PENDING_INTENT_REQUEST_CODE = 246723;
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -31,26 +33,33 @@ public class MainActivity extends AppCompatActivity {
 
                 String channelid = getPackageName() + ".button";
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    CharSequence name           = getString(R.string.notification_name);
-                    String description          = getString(R.string.notification_desc);
-                    int importance              = NotificationManager.IMPORTANCE_HIGH;
-                    NotificationChannel channel = new NotificationChannel(channelid, name, importance);
+                    CharSequence        name         = getString(R.string.notification_name);
+                    String              description  = getString(R.string.notification_desc);
+                    int                 importance   = NotificationManager.IMPORTANCE_HIGH;
+                    NotificationChannel channel      = new NotificationChannel(channelid, name, importance);
+
                     channel.setDescription(description);
                     notificationManager.createNotificationChannel(channel);
-
                 }
 
+                Intent intent = new Intent(context, FullscreenActivity.class);
+                intent.putExtra(getString(R.string.notification_fullscreen_key),"Notification Tapped");
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(v.getContext(), channelid)
+                PendingIntent contentIntent = PendingIntent.getActivity(context, Constants.PENDING_INTENT_REQUEST_CODE,intent,PendingIntent.FLAG_ONE_SHOT);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelid)
                         .setPriority(NotificationManager.IMPORTANCE_DEFAULT)//importance only affects versions 24 -> 26
                         .setContentTitle(getString(R.string.notification_button_title))
                         .setContentText(getString(R.string.notification_button_message))
-                        .setColor(context.getResources().getColor(R.color.colorPrimary))  //icon color in notificaton bar
+                        .setColor(context.getResources().getColor(R.color.colorPrimary))
+                        .setContentIntent(contentIntent)//icon color in notification bar
                         .setSmallIcon(android.R.drawable.ic_dialog_alert);
 
+
                 notificationManager.notify(Constants.NOTIFY_ID, builder.build());
-                //extract notificatonID (1) as a constant (usually in a constant class)
+                //extract notificationID (1) as a constant (usually in a constant class)
                 //extract all strings as string resources
+
 
             }
         });
