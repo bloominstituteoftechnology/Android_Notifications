897bf870fbfda433f1ed9f5874ed32ccd681b2dd
VivekV95 <vivekv210@gmail.com>
2019-03-28 14:48:35 -0500

    Clicking button sends notification

--- app/src/main/java/com/vivekvishwanath/android_notifications/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_notifications/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vivekvishwanath.android_notifications;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -7,11 +8,13 @@ import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
     Button notificationButton;
+    static final int TEXT_NOTIFICATION_ID = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = this;
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationButton = findViewById(R.id.notification_button);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = "Notification Test";
@@ -29,5 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
             notificationManager.createNotificationChannel(channel);
         }
+
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, getPackageName()).
+                        setPriority(NotificationManager.IMPORTANCE_HIGH).
+                        setContentTitle("Test Notification")
+                        .setContentText("This is a test notification")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setColor(getResources().getColor(android.R.color.holo_blue_dark))
+                        .setDefaults(Notification.DEFAULT_ALL);
+
+                notificationManager.notify(MainActivity.TEXT_NOTIFICATION_ID, builder.build());
+            }
+        });
+
     }
+
 }
