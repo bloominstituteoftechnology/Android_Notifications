6993fa00a1291e902fb4269f4baf0f4ba80f5765
VivekV95 <vivekv210@gmail.com>
2019-03-28 14:31:17 -0500

    Created a notification manager and registered channel

--- app/src/main/java/com/vivekvishwanath/android_notifications/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_notifications/MainActivity.java
@@ -1,13 +1,33 @@
 package com.vivekvishwanath.android_notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+    Button notificationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationButton = findViewById(R.id.notification_button);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notification Test";
+            String description = "This channel is used to test sending a notification";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(getPackageName(), name, importance);
+
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
