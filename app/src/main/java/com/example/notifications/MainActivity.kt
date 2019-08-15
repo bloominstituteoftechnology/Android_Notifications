60ae675f46c347abd891cbaba4da9db180923ba5
TylerChristian711 <tylerc71197@gmail.com>
2019-08-15 18:47:06 -0400

    add channel

--- app/src/main/java/com/example/notifications/MainActivity.kt
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.getSystemService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Get_Notification.setOnClickListener {p0 ->
-            
+            val notificationManger = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channelId = "$packageName.projectchannel"
 
-        }
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+               val name = "Notification Channel"
+               val importance = NotificationManager.IMPORTANCE_HIGH
+               val description = "What comes up when you get notification"
+               val channel = NotificationChannel(channelId,name,importance)
+                channel.description = description
+            }
+      }
     }
 }
