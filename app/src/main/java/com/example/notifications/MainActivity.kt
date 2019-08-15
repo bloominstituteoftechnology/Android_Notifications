cdb76fd78fdf0ff5bac261df047b740cfd0565c7
GCGsauce <gattigcg1@gmail.com>
2019-08-15 18:25:51 -0400

    first

---
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val i = ""
+    }
+}
