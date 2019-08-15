d3707fcfbbb48e3f690268e46080ec979fde7f38
GCGsauce <gattigcg1@gmail.com>
2019-08-15 18:53:34 -0400

    f

--- app/src/main/java/com/example/notifications/MainActivity.kt
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.notifications
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val i = ""
+
+        notification_btn.setOnClickListener {
+
+        }
     }
 }
