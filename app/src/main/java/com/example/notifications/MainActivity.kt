d8fc3fd7ff9620435711e2abe5588f0d2de09592
GCGsauce <gattigcg1@gmail.com>
2019-08-15 22:23:08 -0400

    Made notification for main

--- app/src/main/java/com/example/notifications/MainActivity.kt
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -7,12 +7,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        const val NOTIFICATION_ID = 87
+    }
+
+    private val notification = Notification(this, MainActivity::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         notification_btn.setOnClickListener {
-
+            notification.notify(NOTIFICATION_ID, getString(R.string.notification_title_main),
+                getString(R.string.notification_content_main))
         }
+
     }
 }
---
+++ app/src/main/java/com/example/notifications/Notification.kt
@@ -0,0 +1,55 @@
+package com.example.notifications
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class Notification<T>(private val context: Context, private val actingClass : Class<T>,
+                      private val icon : Int = R.drawable.ic_launcher_foreground){
+
+    private val channelId = "{$context.packageName}.simplechannel"
+    var channelImportance = NotificationManager.IMPORTANCE_DEFAULT
+    var notificationPriority =  NotificationCompat.PRIORITY_DEFAULT
+    var toAutoCancel = true
+
+    fun notify(classID : Int = -1,  contentTitle : String = "", contentText : String = ""){
+
+        if(classID != -1) { //needs a class ID to call a notification
+
+            val contentIntent = Intent(context, actingClass)
+            val pendingContentIntent = PendingIntent.getActivity(context, 0, contentIntent,
+                PendingIntent.FLAG_ONE_SHOT)
+
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            createNotificationChannel(notificationManager)
+
+            val notificationBuilder = NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(icon)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setPriority(notificationPriority)
+                .setDeleteIntent(pendingContentIntent)
+                .setAutoCancel(toAutoCancel)
+
+            notificationManager.notify(classID, notificationBuilder.build())
+        }
+    }
+
+    private fun createNotificationChannel(notificationManager : NotificationManager){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.channel_name)
+            val descriptionT = context.getString(R.string.channel_description)
+
+            val channel = NotificationChannel(channelId, name, channelImportance).apply {
+                description = descriptionT
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
