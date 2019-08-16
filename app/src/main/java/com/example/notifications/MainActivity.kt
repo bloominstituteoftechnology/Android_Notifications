dd594749213dcaada2291662775c07726d2eb627
GCGsauce <gattigcg1@gmail.com>
2019-08-15 22:24:46 -0400

    Made notification for main

--- app/src/main/java/com/example/notifications/MainActivity.kt
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -11,16 +11,14 @@ class MainActivity : AppCompatActivity() {
         const val NOTIFICATION_ID = 87
     }
 
-    private val notification = Notification(this, MainActivity::class.java)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         notification_btn.setOnClickListener {
+            val notification = Notification(this, MainActivity::class.java)
             notification.notify(NOTIFICATION_ID, getString(R.string.notification_title_main),
                 getString(R.string.notification_content_main))
-        }
-
+        }   
     }
 }
